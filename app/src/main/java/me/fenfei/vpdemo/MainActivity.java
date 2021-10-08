fc751e8aebe0e767caebb1f1c3f9828631ea0923
周玉岩 <1774329934@qq.com>
2021-10-08 15:35:10 +0800

    代码问题测试

--- app/src/main/java/me/fenfei/vpdemo/MainActivity.java
+++ app/src/main/java/me/fenfei/vpdemo/MainActivity.java
@@ -2,15 +2,15 @@ package me.fenfei.vpdemo;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
-
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,9 +28,22 @@ public class MainActivity extends AppCompatActivity {
         mViewPager2 = findViewById(R.id.main_vp2);
         mTabLayout = findViewById(R.id.main_tab);
         mFragments = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            mFragments.add(VpFragment2.getInstance(String.valueOf(i)));
+
+        mFragments.add(EmptyFragment.getInstance("0"));
+
+        if (false) {
+            mFragments.add(EmptyFragment.getInstance("1"));
+        } else {
+            //下面的这个不管使用是vp还是rv都会存在问题
+            if (false) {
+                mFragments.add(VpFragment.getInstance("1"));
+            } else {
+                mFragments.add(RVFragment.getInstance("1"));
+            }
         }
+        mFragments.add(EmptyFragment.getInstance("2"));
+        mFragments.add(EmptyFragment.getInstance("3"));
+        mFragments.add(EmptyFragment.getInstance("4"));
         Vp2Adapter adapter = new Vp2Adapter(this);
         mViewPager2.setAdapter(adapter);
         adapter.setFragments(mFragments);
