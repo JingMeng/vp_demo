c7208baf8bc24c9f6b79a7aa324953ffaab36b19
周玉岩 <1774329934@qq.com>
2021-10-09 14:51:12 +0800

    抬高版本也是不起作用的

--- app/src/main/java/me/fenfei/vpdemo/MainActivity.java
+++ app/src/main/java/me/fenfei/vpdemo/MainActivity.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 /**
@@ -45,6 +48,21 @@ public class MainActivity extends AppCompatActivity {
                 mFragments.add(RVFragment.getInstance("1"));
             }
         }
+
+        RecyclerView.SmoothScroller smoothScroller = new LinearSmoothScroller(this) {
+            @Override
+            protected int getVerticalSnapPreference() {
+                return LinearSmoothScroller.SNAP_TO_START;
+            }
+
+            @Override
+            protected int getHorizontalSnapPreference() {
+                return LinearSmoothScroller.SNAP_TO_START;
+            }
+        };
+//        smoothScroller.setTargetPosition(position);
+
+
         mFragments.add(EmptyFragment.getInstance("2"));
         mFragments.add(EmptyFragment.getInstance("3"));
         mFragments.add(EmptyFragment.getInstance("4"));
@@ -52,6 +70,48 @@ public class MainActivity extends AppCompatActivity {
         mViewPager2.setAdapter(adapter);
         adapter.setFragments(mFragments);
         mViewPager2.setUserInputEnabled(false);
+
+
+        for (int i = 0; i < 5; i++) {
+            TabLayout.Tab tab = mTabLayout.newTab();
+            tab.setText(i + "-tab");
+            mTabLayout.addTab(tab);
+        }
+
+        if (false){
+            mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    int position = tab.getPosition();
+//                smoothScroller.setTargetPosition(position);
+//                ((RecyclerView) mViewPager2.getChildAt(0)).getLayoutManager().startSmoothScroll(smoothScroller);
+
+//                这个是没有动画效果的
+                    //https://blog.csdn.net/aiyoufang/article/details/82992115
+                    RecyclerView child = (RecyclerView) mViewPager2.getChildAt(0);
+                    if (false) {
+                        child.scrollToPosition(position);
+                    } else {
+                        LinearLayoutManager mLayoutManager = (LinearLayoutManager) child.getLayoutManager();
+                        mLayoutManager.scrollToPositionWithOffset(position, 0);
+                    }
+                    if (false) {
+                        mViewPager2.setCurrentItem(position);
+                    }
+
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+        }else {
         new TabLayoutMediator(mTabLayout,
                 mViewPager2,
                 false,
@@ -59,12 +119,13 @@ public class MainActivity extends AppCompatActivity {
                 (tab, position) -> {
                     tab.setText(position + "-tab");
                 }).attach();
-
+        }
         mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels);
             }
         });
+
     }
 }
